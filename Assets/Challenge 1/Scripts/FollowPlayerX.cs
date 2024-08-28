2ed8bea66bc5ef288a3d994652385a0086e1c9a6
byAliens4Aliens <noureldin@live.co.uk>
2024-08-29 01:39:43 +0300

    First Commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    // Adjusted starting point for camera
+    private Vector3 offset= new Vector3(21, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Added main camera offset
+        transform.position = plane.transform.position + offset;
+    }
+}
